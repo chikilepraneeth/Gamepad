784d39287616b62dbf0d37230b2ba2a60155bc4a
chikilepraneeth <146931676+chikilepraneeth@users.noreply.github.com>
2025-11-19 18:04:33 +0100

    Update app.py

--- hasy-symbol-api/app.py
+++ hasy-symbol-api/app.py
@@ -300,3 +300,5 @@ def predict_matrix(payload: MatrixInput):
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("app:app", host="0.0.0.0", port=int(os.environ.get("PORT", 8000)), reload=True)
+
+
